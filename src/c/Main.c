c9e48aa8d984355bd6d7d67c47b49048f7ffd1bb
Jason <jason.madden8@mail.dcu.ie>
2017-05-05 14:53:06 +0100

    Added the date to the WatchFace

--- src/c/Main.c
+++ src/c/Main.c
@@ -1,6 +1,7 @@
 #include <pebble.h>
 static Window *s_main_window;
 static TextLayer *s_time_text;
+static TextLayer *s_date_text;
 static GFont s_time_font;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
@@ -10,9 +11,12 @@ static void update_time() {
   struct tm*tick_time = localtime(&temp);
   
   //build the string of the time
-  static char s_buffer[8];
-  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M": "%I:%M", tick_time);
+  static char s_other[80];
+  static char s_buffer[80];
+  strftime(s_buffer, sizeof(s_buffer), "%H:%M", tick_time);
+  strftime(s_other, sizeof(s_buffer), "%B %d", tick_time);
   text_layer_set_text(s_time_text, s_buffer);
+  text_layer_set_text(s_date_text, s_other);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -33,23 +37,36 @@ static void main_window_load(Window *window) {
   
   //create the textLayer with the specific bounds
   s_time_text = text_layer_create(
-    GRect(0, PBL_IF_ROUND_ELSE(58, 100), bounds.size.w, 50)
+    GRect(0, PBL_IF_ROUND_ELSE(20, 20), bounds.size.w, 50)
   );
   
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_BATMAN_FONT_30));
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_BATMAN_FONT_25));
   
   //add specifics of the text
   text_layer_set_font(s_time_text, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_text, GTextAlignmentCenter);
   text_layer_set_font(s_time_text, s_time_font);
   
+  //create teh textlayer for date with specific bounds
+  s_date_text = text_layer_create(
+    GRect(0, PBL_IF_ROUND_ELSE(55, 20), bounds.size.w, 50)
+  );
+  
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_BATMAN_FONT_25));
+  
+  //add specifics of the text
+  text_layer_set_text_alignment(s_date_text, GTextAlignmentCenter);
+  text_layer_set_font(s_date_text, s_time_font);
+  
   //add text to the window layer
   layer_add_child(window_layer, text_layer_get_layer(s_time_text));
+  layer_add_child(window_layer, text_layer_get_layer(s_date_text));
 }
 
 static void main_window_unload(Window *window) {
   //destroy text layer
   text_layer_destroy(s_time_text);
+  text_layer_destroy(s_date_text);
   gbitmap_destroy(s_background_bitmap);
   bitmap_layer_destroy(s_background_layer);
 }
