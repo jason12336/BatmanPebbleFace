3d83c1cb1e6989a81f54ba4275b150e19a805760
Jason <jason.madden8@mail.dcu.ie>
2017-05-05 02:37:00 +0100

    Image now displays

---
+++ src/c/Main.c
@@ -0,0 +1,80 @@
+#include <pebble.h>
+static Window *s_main_window;
+static TextLayer *s_time_text;
+static GFont s_time_font;
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
+
+static void update_time() {
+  time_t temp = time(NULL);
+  struct tm*tick_time = localtime(&temp);
+  
+  //build the string of the time
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M": "%I:%M", tick_time);
+  text_layer_set_text(s_time_text, s_buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
+static void main_window_load(Window *window) {
+  //get information of the size of the pebble;
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+  
+  //process and draw the image
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BATMAN_LOGO);
+  s_background_layer = bitmap_layer_create(GRect(0, 60, bounds.size.w, 100));
+  bitmap_layer_set_compositing_mode(s_background_layer, GCompOpSet);
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+  
+  //create the textLayer with the specific bounds
+  s_time_text = text_layer_create(
+    GRect(0, PBL_IF_ROUND_ELSE(58, 100), bounds.size.w, 50)
+  );
+  
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_BATMAN_FONT_30));
+  
+  //add specifics of the text
+  text_layer_set_font(s_time_text, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_time_text, GTextAlignmentCenter);
+  text_layer_set_font(s_time_text, s_time_font);
+  
+  //add text to the window layer
+  layer_add_child(window_layer, text_layer_get_layer(s_time_text));
+}
+
+static void main_window_unload(Window *window) {
+  //destroy text layer
+  text_layer_destroy(s_time_text);
+  gbitmap_destroy(s_background_bitmap);
+  bitmap_layer_destroy(s_background_layer);
+}
+
+static void init() {
+  //create the window
+  s_main_window = window_create();
+  //set the load and unload handlers
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+  //actually be able to see the window
+  window_stack_push(s_main_window, true);
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+}
+
+static void deinit() {
+  //destroy window
+  window_destroy(s_main_window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
